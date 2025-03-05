f973141d029aae9ddbdf851bbc79a187bd17b4b2
SumitParira <parirasumit@gmail.com>
2025-03-06 04:45:53 +0530

    done js upto query selector

---
+++ index.js
@@ -0,0 +1,27 @@
+const typing_ground  =document.querySelector('#textarea');
+const btn = document.querySelector('#btn');
+const score = document.querySelector('#score');
+const show_sentence = document.querySelector('#showSentence');
+
+let startTime,endTime,totalTimeTaken;
+
+const sentences = [
+    'The quick brown fox jumos over the lazy dog 1',
+    'the quick brown fox jumps over the lazy dog 2',
+    'The quicK briwN DOX HIMOS OBTS hd a fohq w3'
+]
+
+btn.addEventListener('click',()=>{
+    switch(btn.innerText.tolowerCase()){
+        case "start":
+            typing_ground.removeAttribute('disabled');
+            startTyping();
+            break;
+
+        case "done":
+            typing_ground.setAttribute('disabled',true);
+            endTypingTest();
+            break;
+
+    }
+});
