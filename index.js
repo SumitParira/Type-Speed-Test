636091f3742d18da1e9801320f63591e9f9f1431
SumitParira <parirasumit@gmail.com>
2025-03-23 03:00:54 +0530

    part 1 of this is completed changes has done in js

--- index.js
+++ index.js
@@ -11,10 +11,58 @@ const sentences = [
     'The quicK briwN DOX HIMOS OBTS hd a fohq w3'
 ]
 
+//step 5
+
+const calculateTypingSpeed = (totalTimeTaken) => {
+    let totalWords = typing_ground.value.trim();
+    let actualWords = totalWords === '' ? 0 : totalWords.split(" ").length;
+
+
+    if(actualWords !== 0){
+        let typing_speed = (actualWords/totalTimeTaken)*60;
+        typing_speed = Math.round(typing_speed);
+        score.innerHTML = `Your typing speed is ${typing_speed} words per minute & you wrote ${actualWords} words & time taken ${totalTimeTaken} sec `;
+
+    }else{
+        score.innerHTML = "Your Typing speed is 0  words per minutes & time taken $(time_taken) sec";
+
+    }
+}
+
+// step 4
+
+const endTypingTest = () => {
+    btn.innerText = "Start";
+
+    let date = new Date();
+    endTime = date.getTime();
+
+    totalTimeTaken = (endTime - startTime)/1000;
+
+    console.log(totalTimeTaken);
+
+    calculateTypingSpeed(totalTimeTaken);
+
+    show_sentence.innerHTML="";
+    typing_ground.value="";
+    
+
+}
+
+
+//step 3
+
 const startTyping = () => {
     
-    let randomNumber  = Math.floor(Math.random());
+    let randomNumber  = Math.floor(Math.random()*sentences.length);
     console.log(randomNumber);
+
+    show_sentence.innerHTML=sentences[randomNumber];
+
+    let date= new Date();
+    startTime = date.getTime();
+
+    btn.innerText= "Done";
     
 }
 
